b6502f503428fcbe28228caf32baaa2a29e42735
Tero Tikkanen <tero.tikkanen@gmail.com>
2016-09-07 10:34:35 +0300

    Change to Middleman, redesign

---
+++ source/javascripts/all.js
@@ -0,0 +1,7 @@
+//= require config
+//= require vendor/mqttws31
+//= require jquery
+//= require bootstrap-sprockets
+//= require moment
+//= require chartist
+//= require main
---
+++ source/javascripts/config.js
@@ -0,0 +1,19 @@
+// host = '127.0.0.1';	// hostname or IP address
+host = 'fvh.rwqr.org';	// hostname or IP address
+//host = '172.24.1.1';	// hostname or IP address
+port = 9001;
+topic = '#';		// topic to subscribe to
+useTLS = false;
+username = null;
+password = null;
+username = "web";
+password = "iotdemo";
+
+// path as in "scheme:[//[user:password@]host[:port]][/]path[?query][#fragment]"
+//    defaults to "/mqtt"
+//    may include query and fragment
+//
+// path = "/mqtt";
+// path = "/data/cloud?device=12345";
+
+cleansession = true;
